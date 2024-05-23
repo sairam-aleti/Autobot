fff81b6fbe43dc8562a3554c551cdeafdcec0a9a
Sai Ram <sairamreddyaleti99@gmail.com>
2024-05-23 23:50:44 +0530

    files

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
